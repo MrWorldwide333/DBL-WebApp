2e0df2dbd488709b406a4911bdb6158a1919d9a6
tgidan <45127217+tgidan@users.noreply.github.com>
2020-06-14 12:44:36 +0200

    Update dbl_flask.py

--- dbl_flask.py
+++ dbl_flask.py
@@ -7,7 +7,8 @@ from bokeh.application.handlers.function import FunctionHandler
 from bokeh.layouts import column, layout, row
 from bokeh.models import Slider, Div, CheckboxGroup, MultiSelect, Select, FileInput
 from bokeh.plotting import figure, ColumnDataSource, show, output_file
-
+from bokeh.models.callbacks import CustomJS
+from bokeh.models import ImageURL 
 from PIL import Image
 import os
 import numpy as np
@@ -31,16 +32,26 @@ def about():
     #Making
     #THIS CELL IS THE ACTUAL CODE, THE OTHER CELLS ARE FOR PRACTICE!!!!!!!!
     #Code that is commented out in this cell is old code, it's there as a backup for if all fails
+
+
     image_name="01_Antwerpen_S1.jpg"
     image_location = "./static/images/MetroMapsEyeTracking/stimuli/{}".format(image_name)
     image= Image.open(image_location) #Opens the image location using the Image library
     image_width, image_height = image.size #image.size is width, height
-    
+    source_image = ColumnDataSource(dict(
+            url = ["./static/images/MetroMapsEyeTracking/stimuli/{}".format(image_name)],
+            x  = [0],
+            y  = [image_height],
+            w  = [image_width],
+            h  = [image_height]
+        ))
     
     Eyetracking_data = pd.read_csv("all_fixation_data_cleaned_up.csv", encoding='latin1', sep="\t")
     Eyetracking_data
     df = Eyetracking_data.copy()
     
+    factor=0.05 #Factor to make non outlier Fixationtime values into the right radius size
+
     def stimuliListMaker(dataframe): #Makes a list with all stimulinames for 
         images=dataframe["StimuliName"].unique().copy()
         menu=[]
@@ -64,7 +75,6 @@ def about():
 #         return active_list
     
     def get_data(df, figure, users=["Everyone"]):
-        factor=0.05 #Factor to make non outlier Fixationtime values into the right radius size
         if users[0] == "Everyone":
             X = df["MappedFixationPointX"][df["StimuliName"] == figure] #Select every x-coordinate from the chosen map
             Y = image_height - df["MappedFixationPointY"][df["StimuliName"] == figure] #Select every y-coordinate from the chosen map
@@ -131,69 +141,77 @@ def about():
 
         #Creation of the figure
         plot = figure(x_range=(0,image_width), y_range=(0,image_height), tooltips=Tooltips, tools=Tools)
-        plot.image_url(url=[image_location], x=0, y=image_height, w=image_width, h=image_height)
+        
+        
+        image1 = ImageURL(url="url", x="x", y="y", w="w", h="h", anchor="top_left")
+        plot.add_glyph(source_image, image1)
+
+        # plot.image_url(url=[image_location], x=0, y=image_height, w=image_width, h=image_height)
         plot.circle(x='x', y='y', size='radius', color='color', alpha=0.2, source=source)
         return plot
     
     
     #The update functions:
-    def update_plot_stimuli(attr, old, new):
-#         try: #Will happen if dataset is selected, This is the case the first time because an example dataset is selected
-        image = new #Image is selected value
-        df = pd.read_csv(File_Input.filename, encoding='latin1', sep='\t')
-        data_new = get_data(df, image)             
+#     def update_plot_stimuli(attr, old, new):
+# #         try: #Will happen if dataset is selected, This is the case the first time because an example dataset is selected
+#         image = new #Image is selected value
+#         df = pd.read_csv(File_Input.filename, encoding='latin1', sep='\t')
+#         data_new = get_data(df, image)             
 
-        #Change user selection to users of this plot stimuli and select the previously selected value if it exists.
+#         #Change user selection to users of this plot stimuli and select the previously selected value if it exists.
         
-        user_select.options = userListMaker(df, image)
-        user_select.value = ["Everyone"]
-        source.data = data_new
-#         except: #Will happen if no dataset is selected.
-#             print("error")
+#         user_select.options = userListMaker(df, image)
+#         user_select.value = ["Everyone"]
+#         source.data = data_new
+# #         except: #Will happen if no dataset is selected.
+# #             print("error")
         
     
-    def update_plot_dataset(attr, old, new):
-        try: #Will happen if a dataset is selected
-            dataframe = pd.read_csv(File_Input.filename, encoding='latin1', sep='\t')
+    # def update_plot_dataset(attr, old, new):
+    #     try: #Will happen if a dataset is selected
+    #         dataframe = pd.read_csv(File_Input.filename, encoding='latin1', sep='\t')
             
-            #Get the first image from the list to already show a map for the plot
-            image_name = dataframe["StimuliName"][0] 
-            image_location = "./static/images/MetroMapsEyeTracking/stimuli/{}".format(image_name)
-            image= Image.open(image_location)
-            image_width, image_height = image.size
+    #         #Get the first image from the list to already show a map for the plot
+    #         image_name = dataframe["StimuliName"][0] 
+    #         image_location = "./static/images/MetroMapsEyeTracking/stimuli/{}".format(image_name)
+    #         image= Image.open(image_location)
+    #         image_width, image_height = image.size
             
-            data_new = get_data(dataframe, image_name)
+    #         data_new = get_data(dataframe, image_name)
             
-            #Change the stimuliSelection menu so that only the images from this dataset can be selected
-            stimuliSelect.options=stimuliListMaker(dataframe)
-            stimuliSelect.value=image_name
+    #         #Change the stimuliSelection menu so that only the images from this dataset can be selected
+    #         stimuliSelect.options=stimuliListMaker(dataframe)
+    #         stimuliSelect.value=image_name
             
-            #Change the user_select menu so that it only shows the users for this dataset with the first stimuli of the new dataset. Also resets the chosen value to "Everyone"
-            user_select.options=userListMaker(dataframe, image_name)
-            user_select.value=["Everyone"]
+    #         #Change the user_select menu so that it only shows the users for this dataset with the first stimuli of the new dataset. Also resets the chosen value to "Everyone"
+    #         user_select.options=userListMaker(dataframe, image_name)
+    #         user_select.value=["Everyone"]
             
-            source.data = data_new
+    #         source.data = data_new
         
-        except: #Will happen if no dataset is selected
-            print("error")
+    #     except: #Will happen if no dataset is selected
+    #         print("error")
     
     #Potential user selection code
-    def update_plot_user(attr, old, new):
-        active_list = new
-        dataframe = pd.read_csv(File_Input.filename, encoding='latin1', sep='\t')
-        stimuli = stimuliSelect.value
-        data_new = get_data(dataframe, stimuli, active_list)
-        source.data= data_new
+    # def update_plot_user(attr, old, new):
+    #     active_list = new
+    #     dataframe = pd.read_csv(File_Input.filename, encoding='latin1', sep='\t')
+    #     stimuli = stimuliSelect.value
+    #     data_new = get_data(dataframe, stimuli, active_list)
+    #     source.data= data_new
 
+
+    
     
     #Code needed to make stimuli selector 
     stimuliList = stimuliListMaker(df)
     stimuliSelect = Select(value=image_name, title='Stimuli', options=stimuliList)
-    stimuliSelect.on_change('value', update_plot_stimuli)
+    # stimuliSelect.on_change('value', update_plot_stimuli)
     
+
     #Code needed for file upload system
     File_Input = FileInput(filename="all_fixation_data_cleaned_up.csv", accept=".csv")
-    File_Input.on_change('value', update_plot_dataset)
+    # File_Input.on_change('value', update_plot_dataset)
     
     
     Tools_vis1="pan,wheel_zoom, box_select, tap, reset, save"
@@ -203,12 +221,86 @@ def about():
     user_list = userListMaker(df, image_name)
     user_select = MultiSelect(title="Users:", value=["Everyone"],
                         options=user_list ) #Don't forget to press shift or control (shift for everyone in between, control for selecting specific people) when selecting multiple users
-    user_select.on_change("value", update_plot_user)
+    # user_select.on_change("value", update_plot_user)
     
+
+    stimuliSelect_callback = CustomJS(args=dict(source=source, users=user_select.value, dataframe=ColumnDataSource(df), source_image=source_image, factor=factor), code=
+        """
+            var datadf = dataframe.data
+            var datasource = source.data;
+            var userList = users;
+            var dataimagesource= source_image.data;
+            var new_image = new Image();
+            new_image.onload = function() {
+                dataimagesource['h'][0] = this.height;
+                dataimagesource['w'][0] = this.width;
+                
+            } 
+            var image_height = dataimagesource['h'][0];
+            var image_width = dataimagesource['w'][0];
+
+            var new_stimuli = cb_obj.value;
+            var location= ("./static/images/MetroMapsEyeTracking/stimuli/"+new_stimuli)
+            dataimagesource['url'][0] = location
+
+            var f= factor;
+            //console.log(userList[0]);
+            //console.log(cb_obj.value);
+            //console.log(datasource);
+            //console.log(datadf);
+
+
+            //Empty all lists that are in the source so that we can add new values to them
+            datasource['x']=[];
+            datasource['y']=[];
+            datasource['fixationtime']=[];
+            datasource['radius']=[];
+            datasource['user']=[];
+            datasource['description']=[];
+            datasource['color']=[];
+
+            
+
+            //Add new values to the lists in the source
+            for (var i = 0; i < datadf['StimuliName'].length; i++) { //MAYBE LOOK INTO PANDAS FOR JAVASCRIPT AS IT WOULD BE A LOT CLEANER THAN THIS 
+                if (userList[0] == "Everyone") {
+                    if (datadf['StimuliName'][i] == new_stimuli ) {
+                        datasource['x'].push(datadf['MappedFixationPointX'][i])
+                        datasource['y'].push(image_height - datadf['MappedFixationPointY'][i])
+                        datasource['fixationtime'].push(datadf['FixationDuration'][i])
+
+                        var Radius = datadf['FixationDuration'][i]*f;
+                        if (Radius > 75) {
+                            datasource['radius'].push(75)
+                            datasource['color'].push("RED")
+                        }
+                        else {
+                            datasource['radius'].push(Radius)
+                            datasource['color'].push("BLUE")
+                        }
+                        
+                        datasource['user'].push(datadf['user'][i])
+                        datasource['description'].push(datadf['description'][i])
+                        
+
+                    }
+
+
+
+                }
+            }
+            //console.log(datasource['x']);
+            source.change.emit();
+            source_image.change.emit();
+        """)
+
+    #JS_Callbacks, so basically calling an update function that runs on Javascript -_-
+    stimuliSelect.js_on_change('value', stimuliSelect_callback)
+
     scatterplot = make_plot(Tools_vis1, source)
     controls = column(File_Input, stimuliSelect, user_select)
     layout = row(scatterplot, controls)
-    #doc.add_root(layout)
+    # curdoc.add_root(layout)
     script, div = components(layout, wrap_script=False)
     return render_template('about.html', script=script, div=div)
     #return render_template('about.html')
